06d7c2ba06e760192ada9048d121da1dd4002b82
trandangkhoa5495 <trandangkhoa5495@gmail.com>
2023-04-02 22:05:37 +0700

    uploaf file

---
+++ index.js
@@ -0,0 +1,30 @@
+// bài 1
+let i = 10
+let f = 20.5
+let b = true
+let s = "Hà Nội"
+
+document.write("i="+i)
+document.write("<br/>")
+document.write("f="+f)
+document.write("<br/>")
+document.write("b="+b)
+document.write("<br/>")
+document.write("s="+s)
+document.write("<br/>")
+
+// bài 2
+
+let width=20
+let height=10
+let area= width*height
+document.write("Area="+area)
+document.write("<br/>")
+ 
+// bài 3
+
+const A= Number(prompt("nhập vào số a"));
+const B= Number(prompt("nhập vào số b"));
+
+const divisible= A/B
+ alert("đáp số là:"+divisible)
---
+++ index2.js
@@ -0,0 +1,5 @@
+const width= Number(prompt("Nhập chiều dài:"));
+const height= Number(prompt("nhập chiều rộng:"));
+
+const S = width*height
+document.write("Diện tích hình chữ nhật là:"+ S)
